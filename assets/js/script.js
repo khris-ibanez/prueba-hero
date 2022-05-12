c1ad4fff520389d7b64dcef6c9f7a21d65c3e39d
Christopher Ibanez <christopher.ibanez.b@gmail.com>
2022-05-12 13:50:54 -0400

    version1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,65 @@
 $(document).ready(function(){
     $('#formHero').submit(function(event){
         event.preventDefault();
+        console.log('funciona sumit');
+        $('#mensajeHeroe').text('cargando...');
+        let idHeroe=$('#txtSuperheroe').val();
 
         $.ajax({
             type:"GET",
-            url:`https://superheroapi.com/api/4905856019427443/${idHeroe}`,
+            url:`https://superheroapi.com/api.php/4905856019427443/${idHeroe}`,
             success:function(data){
-                $('#mensajeHero').text('SuperHero Encontrado');
+
+                $('#mensajeHeroe').text('SuperHero Encontrado');
                 console.log(data);
+                $('#imagenHeroe').attr("src",`${data.image.url}`);
+                $('#nombreHeroe').text(`Nombre: ${data.name}`);
+                let lasConexiones = data['connections']['group-affiliation'];
+                $('#conexionesHeroe').text(`Conexiones: ${lasConexiones}`);
+                $('#publicado').text(`publicado por: ${data.biography.publisher}`);
+                $('#ocupacion').text(`Ocupacion: ${data.work.occupation}`);
+                let primeraA = data['biography']['first-appearance'];
+                $('#apa').text(`primera aparicion: ${primeraA}`);
+                $('#altura').text(`Altura: ${data.appearance.height}`);
+                $('#peso').text(`Peso: ${data.appearance.weight}`);
+                $('#alianzas').text(`Alianzas: ${data.biography.aliases}`);
+
+                let inteligencia=`${data.powerstats.intelligence}`;
+                let fuerza=`${data.powerstats.strength}`;
+                let velocidad=`${data.powerstats.speed}`;
+                let durability=`${data.powerstats.durability}`;
+                let power=`${data.powerstats.power}`;
+                let combat=`${data.powerstats.combat}`;
+                
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    theme: "light2",
+                    exportEnabled: true,
+                    animationEnabled: true,
+                    title: {
+                        text: `Estadisticas de ${data.name}`
+                    },
+                    data: [{
+                        type: "pie",
+                        startAngle: 25,
+                        toolTipContent: "<b>{label}</b>: {y}",
+                        showInLegend: "true",
+                        legendText: "{label}",
+                        indexLabelFontSize: 16,
+                        indexLabel: "{label} - {y}",
+                        dataPoints: [
+                            { y: inteligencia, label: "Inteligencia" },
+                            { y: fuerza, label: "fuerza" },
+                            { y: velocidad, label: "velocidad" },
+                            { y: durability, label: "durability" },
+                            { y: power, label: "power" },
+                            { y: combat, label: "combat" },
+                            
+                        ]
+                    }]
+                });
+                chart.render();
             },
+            
             error:function(data){
                 $('#mensajeHeroe').text(`API NO ENCONTRADA: ${data}`);
             },
